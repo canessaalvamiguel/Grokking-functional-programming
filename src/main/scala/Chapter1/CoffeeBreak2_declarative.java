ad6dc14a92b4118a5f126c45967d8fc90a53ac39
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-06-12 10:09:33 -0500

    Add Cofee Break 2 - Chapter 1

---
+++ src/main/scala/Chapter1/CoffeeBreak2_declarative.java
@@ -0,0 +1,41 @@
+package Chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoffeeBreak2_declarative {
+
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        TipCalculator2 tipCalculator = new TipCalculator2();
+
+        names = tipCalculator.addPerson(names,"Miguel");
+        names = tipCalculator.addPerson(names,"Jose");
+        names = tipCalculator.addPerson(names,"Carlos");
+        names = tipCalculator.addPerson(names,"Roberto");
+
+        System.out.println("TipPercentage: " + TipCalculator2.getTipPercentage(names));
+
+        names = tipCalculator.addPerson(names,"Diego");
+        names = tipCalculator.addPerson(names,"Max");
+
+        System.out.println("TipPercentage: " + TipCalculator2.getTipPercentage(names));
+    }
+}
+
+class TipCalculator2{
+    public List<String> addPerson(List<String> names, String name){
+        List<String> updated = new ArrayList<>(names);
+        updated.add(name);
+
+        return updated;
+    }
+
+    public static int getTipPercentage(List<String> names){
+        if (names.size() >  5){
+            return 20;
+        }else if(names.size() > 0){
+            return 10;
+        }else return 0;
+    }
+}
---
+++ src/main/scala/Chapter1/CoffeeBreak2_imperative.java
@@ -0,0 +1,44 @@
+package Chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoffeeBreak2_imperative {
+
+    public static void main(String[] args) {
+        TipCalculator tipCalculator = new TipCalculator();
+
+        tipCalculator.addPerson("Miguel");
+        tipCalculator.addPerson("Jose");
+        tipCalculator.addPerson("Carlos");
+        tipCalculator.addPerson("Roberto");
+
+        System.out.println("TipPercentage: " + tipCalculator.getTipPercentage());
+
+        tipCalculator.addPerson("Diego");
+        tipCalculator.addPerson("Max");
+
+        System.out.println("TipPercentage: " + tipCalculator.getTipPercentage());
+    }
+}
+
+class TipCalculator {
+    private List<String> names = new ArrayList<>();
+    private int tipPercentage = 0;
+    public void addPerson(String name) {
+        names.add(name);
+        if(names.size() > 5) {
+            tipPercentage = 20;
+        } else if(names.size() > 0) {
+            tipPercentage = 10;
+        }
+    }
+    public List<String> getNames() {
+        return names;
+    }
+    public int getTipPercentage() {
+        return tipPercentage;
+    }
+}
+
+
